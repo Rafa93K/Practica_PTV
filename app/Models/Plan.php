d4dc2f890b99545f7ba381f8e523ae08f47cc9ad
DESKTOP-G21EFIE\PTVTELECOM <acoronadoalcalde@gmail.com>
2026-03-03 12:02:02 +0100

    Migraciones y modelos

---
+++ app/Models/Plan.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Plan extends Model
+{
+    protected $fillable = ['name', 'type', 'price', 'description']; //Datos que se guardan en la bd
+
+    //Un plan puede tener muchos productos
+    public function products(): BelongsToMany {
+        return $this->belongsToMany(Product::class);
+    }
+
+    //Un plan puede tener muchos contratos
+    public function contracts(): BelongsToMany {
+        return $this->belongsToMany(Contract::class);
+    }
+}
